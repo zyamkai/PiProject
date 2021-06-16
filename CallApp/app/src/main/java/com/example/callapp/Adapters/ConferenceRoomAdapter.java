266e2167bd4dbf4d7ba39bc9fc2b785d4568e271
zyamkai <szymon.mazur1998@gmail.com>
2021-06-16 19:23:11 +0200

    Update ConferenceRoomAdapter.java

--- CallApp/app/src/main/java/com/example/callapp/Adapters/ConferenceRoomAdapter.java
+++ CallApp/app/src/main/java/com/example/callapp/Adapters/ConferenceRoomAdapter.java
@@ -1,21 +1,17 @@
 package com.example.callapp.Adapters;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.app.Activity;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.callapp.CallActivity;
-import com.example.callapp.ConferenceRoom;
-import com.example.callapp.Models.Conference;
-import com.example.callapp.Models.User;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.callapp.ConferenceActivity;
+import com.example.callapp.Models.ConferenceRoom;
 import com.example.callapp.R;
 
 import java.util.ArrayList;
@@ -23,17 +19,18 @@ import java.util.ArrayList;
 public class ConferenceRoomAdapter extends RecyclerView.Adapter<ConferenceRoomAdapter.ConferenceRoomViewHolder> {
 
     Activity context;
-    ArrayList<Conference> conferenceArrayList;
+    ArrayList<ConferenceRoom> conferenceRoomArrayList;
 
-    public ConferenceRoomAdapter(Activity context, ArrayList<Conference> conferenceArrayList){
+    public ConferenceRoomAdapter(Activity context, ArrayList<ConferenceRoom> conferenceRoomArrayList){
         this.context = context;
-        this.conferenceArrayList = conferenceArrayList;
+        this.conferenceRoomArrayList = conferenceRoomArrayList;
     }
+
 
     @NonNull
     @Override
     public ConferenceRoomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_conference_adapter,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.conference_room,parent,false);
         ConferenceRoomViewHolder conferenceRoomAdapter = new ConferenceRoomViewHolder(view);
 
 
@@ -42,23 +39,42 @@ public class ConferenceRoomAdapter extends RecyclerView.Adapter<ConferenceRoomAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ConferenceRoomAdapter.ConferenceRoomViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ConferenceRoomViewHolder holder, int position) {
+        ConferenceRoom conferenceRoom = conferenceRoomArrayList.get(position);
+        holder.textViewName.setText(conferenceRoom.getConferenceRoomId());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return conferenceRoomArrayList.size();
     }
 
     public class ConferenceRoomViewHolder extends RecyclerView.ViewHolder{
 
         TextView textViewName;
+        Button button, button2;
 
         public ConferenceRoomViewHolder(@NonNull View itemView) {
             super(itemView);
-            textViewName = (TextView)itemView.findViewById(R.id.conference_adapter_itemName);
+            textViewName = (TextView)itemView.findViewById(R.id.ConferenceItemName);
+            button = itemView.findViewById(R.id.ConferenceCallButton);
+            button2 = itemView.findViewById(R.id.ConferenceAddUser);
 
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ConferenceRoom conferenceRoom = conferenceRoomArrayList.get(getAdapterPosition());
+                    ((ConferenceActivity)context).callConference(conferenceRoom);
+                }
+            });
+
+            button2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ConferenceRoom conferenceRoom = conferenceRoomArrayList.get(getAdapterPosition());
+                    ((ConferenceActivity)context).addUserToConferenceRoom(conferenceRoom);
+                }
+            });
         }
     }
 }
